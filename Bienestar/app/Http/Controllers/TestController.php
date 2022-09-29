e0c0a73f12e217768d087d44e88e3d12eb740b35
sceballosp <sceballosp@eafit.edu.co>
2022-09-29 10:36:40 -0500

    Test score

--- Bienestar/app/Http/Controllers/TestController.php
+++ Bienestar/app/Http/Controllers/TestController.php
@@ -18,24 +18,80 @@ class TestController extends Controller
         return view('test.index')->with("viewData", $viewData);
     }
 
-    public function save(Request $request)
-    {
+    public function save(Request $request){
+
+        $module1 = Question::where('module', 1)->get('id');
+        $module1Length = count($module1);
+        $scoreModule1 = 0;
+        for ($i = 0; $i < $module1Length; $i++){
+            $scoreModule1 += request(strval($module1[$i]->id));
+        }
+        
+        $module2 = Question::where('module', 2)->get('id');
+        $module2Length = count($module2);
+        $scoreModule2 = 0;
+        for ($i = 0; $i < $module2Length; $i++){
+            $scoreModule2 += request(strval($module2[$i]->id));
+        }
+        
+        $module3 = Question::where('module', 3)->get('id');
+        $module3Length = count($module3);
+        $scoreModule3 = 0;
+        for ($i = 0; $i < $module3Length; $i++){
+            $scoreModule3 += request(strval($module3[$i]->id));
+        }
+
+        $module4 = Question::where('module', 4)->get('id');
+        $module4Length = count($module4);
+        $scoreModule4 = 0;
+        for ($i = 0; $i < $module4Length; $i++){
+            $scoreModule4 += request(strval($module4[$i]->id));
+        }
+
+        $module5 = Question::where('module', 5)->get('id');
+        $module5Length = count($module5);
+        $scoreModule5 = 0;
+        for ($i = 0; $i < $module5Length; $i++){
+            $scoreModule5 += request(strval($module5[$i]->id));
+        }
+
+        $scoreArray[0] = $scoreModule1;
+        $scoreArray[1] = $scoreModule2;
+        $scoreArray[2] = $scoreModule3;
+        $scoreArray[3] = $scoreModule4;
+        $scoreArray[4] = $scoreModule5;
+        
         $modules = Module::all();
+        $cont = 0;
         foreach($modules as $module) {
+        
             $usermod = new UserMod();
             $usermod->module_id = $module->id;
             $usermod->user_id = Auth::id();
-            $usermod->score = 0;
+            $usermod->score = $scoreArray[$cont];
             $usermod->save();
+            $cont++;
         }
-
-        $total = Question::all()->pluck('id');
-        foreach($total as $i) {
-            $pregunta = request(strval($i));
-            $usermod_id = DB::table("usermod")->where('module_id', strval($i))->pluck('id')->first();
-            $usermod = UserMod::all()->where('id', $usermod_id)->first();
-            $usermod->score += intval($pregunta);
-            $usermod->save();
-        }
+    
+        
+//
+        //$data = [
+        //    ['score'=>$scoreModule1],
+        //    ['score'=>$scoreModule2],
+        //    ['score'=>$scoreModule3],
+        //    ['score'=>$scoreModule4],
+        //    ['score'=>$scoreModule5],
+        //];
+        //
+        //UserMod::insert($data);
+//
+        //    //echo($module1);
+        //    //$module_id = Question::where('id', strval($i))->get('module');
+            //$usermod_id = DB::table("usermod")->where('module_id', $module_id)->pluck('id')->first();
+            //$usermod = UserMod::all()->where('id', $usermod_id)->first();
+            //$usermod->score += intval($pregunta);
+            //$usermod->save();
+        
     }
+
 }
