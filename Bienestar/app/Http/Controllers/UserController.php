483033daf7bef7fb871038947fd5f7304faf2371
Daniela Nino <danielanino194@gmail.com>
2022-10-11 18:51:14 -0500

    UserController modified

--- Bienestar/app/Http/Controllers/UserController.php
+++ Bienestar/app/Http/Controllers/UserController.php
@@ -3,16 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
-class HomeController extends Controller
+class UserController extends Controller
 {
     public function index()
     {
         $viewData = [];
         $user_id = Auth::id();
-        $users = User::where('id', $user_id)->get('patients');
-        $viewData["users"] = $users;
+        $users_id = DB::table('doc_pat')->where('doctor_id', $user_id)->pluck('patient_id')->toArray();
+        $users = [];
+        foreach($users_id as $user_id) {
+            $user = User::findOrFail($user_id);
+            array_push($users, $user);
+        }
+        $viewData['users'] = $users;
         return view('doctor.user.index')->with("viewData", $viewData);
     }
     
