3a5fabe715ee4c83d222c60b752641adc7c420b4
Daniela Nino <danielanino194@gmail.com>
2022-11-15 13:17:32 -0500

    test

---
+++ Bienestar/resources/views/patient/test.blade.php
@@ -0,0 +1,11 @@
+
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-15">
+        <div class="w3-light-grey">
+            <div class="w3-container w3-red w3-padding w3-center" style="width:25%">25%</div>
+            </div>
+        </div>
+    </div>
+  </div>
+</div>
