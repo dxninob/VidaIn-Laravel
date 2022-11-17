9ee14fcf633d07fbe5760d8047ce803bc45887ba
Daniela Nino <danielanino194@gmail.com>
2022-11-17 10:59:20 -0500

    Login blade

--- Bienestar/resources/views/auth/login.blade.php
+++ Bienestar/resources/views/auth/login.blade.php
@@ -1,67 +1,64 @@
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="{{ asset('/css/login.css') }}">
+<script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
 
-<div class="container">
-    <div class="row justify-content-center">
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <h1>Ingresar</h1>
+</div>
+<form method="POST" action="{{ route('login') }}">
+@csrf
+    <div class="row">
         <div class="col-md-8">
-        <div class="card-body">
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" placeholder="Correo Electrónico" required autocomplete="email" autofocus>
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                
+            </div>
 
-                <div class="row mb-3">
-                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Correo') }}</label>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Contraseña" required autocomplete="current-password">
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
 
-                    <div class="col-md-6">
-                        <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <div class="form-check">
+                        <input class="form-check-input check" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <label class="form-check-label" for="remember">
+                            {{ __('Recordarme') }}
+                        </label>
                     </div>
                 </div>
+            </div>
 
-                <div class="row mb-3">
-                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña') }}</label>
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                    <button type="submit" class="btn btn-primary bg-secondary">
+                        <b>{{ __('INGRESAR') }}</b>
+                    </button>
 
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                    @endif
                 </div>
-
-                <div class="row mb-3">
-                    <div class="col-md-6 offset-md-4">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Recordarme') }}
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-primary bg-secondary">
-                            {{ __('Ingresar') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('¿Olvidaste tu contraseña?') }}
-                            </a>
-                        @endif
-                    </div>
-                </div>
-            </form>
-        </div>
+            </div>
         </div>
     </div>
+</form>
+<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+    <h3>Al registrarte en BIENESTAR, aceptas nuestros términos <br> y políticas de privacidad.</h3>
 </div>
-
