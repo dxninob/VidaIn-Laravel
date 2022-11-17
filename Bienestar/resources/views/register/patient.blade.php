1d38025261a3e0db21ffb9ab6beeb660b8d6d4d3
Daniela Nino <danielanino194@gmail.com>
2022-11-17 14:18:31 -0500

    Registros paciente y doctor

--- Bienestar/resources/views/register/patient.blade.php
+++ Bienestar/resources/views/register/patient.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 @section('content')
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="{{ asset('/css/registroDoctor.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/registroPatient.css') }}">
 <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
 
 <body>
     <form method="POST" action="{{ route('register') }}">
         @csrf
         <div class="container">
-            
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 red">
                 <h1>Crea tu perfil</h1>
             </div>
@@ -43,13 +42,24 @@
                 <div class="row">
                     <input id="document" type="number" class="form-control @error('document') is-invalid @enderror" name="document" value="{{ old('document') }}" placeholder="Número de Identificación" required autocomplete="document" autofocus>
                 </div>
+
+                <div class="row">
+                    <input id="birthday" type="date" class="form-control @error('birthday') is-invalid @enderror" name="birthday" value="{{ old('birthday') }}" placeholder="Fecha de nacimiento" required autocomplete="birthday" autofocus>
+                </div>
             </div>
 
             <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 red">
                 <div class="row">
-                    <input id="profession" type="text" class="form-control @error('profession') is-invalid @enderror" name="profession" value="{{ old('profession') }}" placeholder="Profesión" required autocomplete="profession" autofocus>
+                    <input id="phone" type="number" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ old('phone') }}" placeholder="Número de teléfono" required autocomplete="phone" autofocus>
                 </div>
 
+                <div class="row">
+                    <input id="namePatient" type="text" class="form-control @error('namePatient') is-invalid @enderror" name="namePatient" value="{{ old('namePatient') }}" placeholder="Nombre del paciente a su cargo" required autocomplete="namePatient" autofocus>
+                </div>
+
+                <div class="row">
+                    <input id="documentPatient" type="number" class="form-control @error('documentPatient') is-invalid @enderror" name="documentPatient" value="{{ old('documentPatient') }}" placeholder="Documento del paciente a su cargo" required autocomplete="documentPatient" autofocus>
+                </div>
                 <div class="row">
                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" placeholder="Contraseña" required autocomplete="new-password">
                     @error('password')
@@ -63,16 +73,17 @@
                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" placeholder="Confirmar Contraseña" required autocomplete="new-password">
                 </div>
 
-                <input id="doctor" type="hidden" name="role" value="doctor">
+                <input id="cuidador" type="hidden" name="role" value="cuidador">
             </div>
 
             <div class="container2">
-                <button type="submit" id="boton1">
+                    <button type="submit" id="boton1">
                         {{ __('Registrarse') }}
-                </button>
-                <br>
+                    </button>
             </div>
-            <p>Al registrarte en BIENESTAR, aceptas nuestros <b>términos y políticas de privacidad.</b></p>
+            <div>
+                <p>Al registrarte en BIENESTAR, aceptas nuestros <b>términos y políticas de privacidad.</b></p>
+            </div>
         </div>
     </form>
 </body>
