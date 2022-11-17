7510461f7285211633d6c550cd31717c7b7e78e8
Samuel Ceballos <sceballosp@eafit.edu.co>
2022-11-17 00:47:05 -0500

    vistas test

--- Bienestar/resources/views/test/index8.blade.php
+++ Bienestar/resources/views/test/index8.blade.php
@@ -1,47 +1,69 @@
 @extends('layouts.app')
-
 @section('content')
-<div class="container">
-  <div class="row justify-content-center">
-    <div class="col-md-15">
-      <form action="{{ route('test.save') }}" method="POST">
-        @csrf
-        <table class="default">
-          <tr>
-            <td style="text-align:center"> <strong> Pregunta </strong></td>
-            <td style="text-align:center"> <strong> Totalmente en desacuerdo </strong></td>
-            <td style="text-align:center"> <strong> En desacuerdo </strong></td>
-            <td style="text-align:center"> <strong> Ni de acuerdo ni en desacuerdo </strong></td>
-            <td style="text-align:center"> <strong> De acuerdo </strong></td>
-            <td style="text-align:center"> <strong> Totalmente de acuerdo </strong></td>
-          </tr>
-          @foreach ($viewData['questions22-24'] as $question)
-          <tr>
-            <td>{{ $question->id}}. {{ $question->question }}</td>
-            @if($question->id == 23 || $question->id == 24)
-            <td style="text-align:center"><input id="1" type="radio" name="{{ $question->id }}" value="1" required /></td>
-            <td style="text-align:center"><input id="2" type="radio" name="{{ $question->id }}" value="2" /></td>
-            <td style="text-align:center"><input id="3" type="radio" name="{{ $question->id }}" value="3" /></td>
-            <td style="text-align:center"><input id="4" type="radio" name="{{ $question->id }}" value="4" /></td>
-            <td style="text-align:center"><input id="5" type="radio" name="{{ $question->id }}" value="5" /></td>
-            @else
-            <td style="text-align:center"><input id="1" type="radio" name="{{ $question->id }}" value="5" required /></td>
-            <td style="text-align:center"><input id="2" type="radio" name="{{ $question->id }}" value="4" /></td>
-            <td style="text-align:center"><input id="3" type="radio" name="{{ $question->id }}" value="3" /></td>
-            <td style="text-align:center"><input id="4" type="radio" name="{{ $question->id }}" value="2" /></td>
-            <td style="text-align:center"><input id="5" type="radio" name="{{ $question->id }}" value="1" /></td>
-            @endif
-          </tr>
-          @endforeach
-        </table>
-        <br>
-        <div class="row mb-0">
-          <div>
-            <button type="submit" class="btn btn-primary bg-secondary" name="index8" value="8">{{ __('Siguiente') }}</button>
-          </div>
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
+  <link href="{{ asset('/css/test.css') }}" rel="stylesheet" />
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
+  <title>@yield('title', 'BienEstar')</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="pad col-md-15">
+        <h3>Progreso: 56%</h3>
+        <div class="progressbar" id="progressbar8">
+          <div></div>
         </div>
-      </form>
+        <form action="{{ route('test.save') }}" method="POST">
+          @csrf
+          <table class="table custom-table">
+            <thead>
+              <tr>
+                <th scope="col" style="text-align:center"> Número </th>
+                <th scope="col" style="text-align:center"> Pregunta </th>
+                <th scope="col" style="text-align:center"> Totalmente en desacuerdo </th>
+                <th scope="col" style="text-align:center"> En desacuerdo </th>
+                <th scope="col" style="text-align:center"> Ni de acuerdo ni en desacuerdo </th>
+                <th scope="col" style="text-align:center"> De acuerdo </th>
+                <th scope="col" style="text-align:center"> Totalmente de acuerdo </th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($viewData['questions22-24'] as $question)
+              <tr scope="row">
+                <td>{{ $question->id }}.</td>
+                <td>{{ $question->question }}</td>
+                @if($question->id == 23 || $question->id == 24)
+                <td><input id="1" type="radio" name="{{ $question->id }}" value="1" required /></td>
+                <td><input id="2" type="radio" name="{{ $question->id }}" value="2" /></td>
+                <td><input id="3" type="radio" name="{{ $question->id }}" value="3" /></td>
+                <td><input id="4" type="radio" name="{{ $question->id }}" value="4" /></td>
+                <td><input id="5" type="radio" name="{{ $question->id }}" value="5" /></td>
+                @else
+                <td><input id="1" type="radio" name="{{ $question->id }}" value="5" required /></td>
+                <td><input id="2" type="radio" name="{{ $question->id }}" value="4" /></td>
+                <td><input id="3" type="radio" name="{{ $question->id }}" value="3" /></td>
+                <td><input id="4" type="radio" name="{{ $question->id }}" value="2" /></td>
+                <td><input id="5" type="radio" name="{{ $question->id }}" value="1" /></td>
+                @endif
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+          <br>
+          <div class="row mb-0">
+            <div class="button">
+              <button type="submit" class="btn btn-primary bg-secondary" style="float: right;" name="index8" value="8">{{ __('Siguiente') }}</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   </div>
-</div>
+</body>
 @endsection
