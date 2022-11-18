5cbe1149eb6d6f2ded4933e3d9ee70878f17eeaf
Daniela Nino <danielanino194@gmail.com>
2022-11-18 08:55:55 -0500

    Botones cuidador alineados

--- Bienestar/resources/views/cuidador/index.blade.php
+++ Bienestar/resources/views/cuidador/index.blade.php
@@ -13,26 +13,33 @@
         <p>Hola Acompañanante</p>
     </div>
     <div class="introduccion2">
-        <p>Acá tendras la opción de ver el progreso de Marta y hablar directamente con el  comite para consultar cualquier duda que tengas.</p>
+        <p>Acá tendras la opción de ver el progreso de Marta y hablar directamente con el comite para consultar cualquier duda que tengas.</p>
     </div>
-    <div class="row">
+    <div class="alinear">
         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="boton">
-                <a id="boton" href="#">Progreso de actividades</a>
+            <div class="espacio">
+
+                <div class="boton">
+                    <a id="boton" href="#">Progreso de<br>actividades</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row">
+
         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="boton">
-                <a id="boton" href="#">Resultados del test</a>
+            <div class="espacio">
+
+                <div class="boton">
+                    <a id="boton" href="#">Resultados<br>del test</a>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="row">
+
         <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
-            <div class="boton">
-                <a id="boton" href="#">Información de módulos</a>
+            <div class="espacio">
+
+                <div class="boton">
+                    <a id="boton" href="#">Información de<br>módulos</a>
+                </div>
             </div>
         </div>
     </div>
