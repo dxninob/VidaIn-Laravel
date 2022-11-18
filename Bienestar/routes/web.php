3fd651e409e6fc1bd6200ea9fe901f633058f6a7
Daniela Nino <danielanino194@gmail.com>
2022-11-18 04:31:28 -0500

    Rutas terminadas

--- Bienestar/routes/web.php
+++ Bienestar/routes/web.php
@@ -14,21 +14,12 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-// Route::get('/', 'App\Http\Controllers\HomeController@home')->name("home.home");
-// Route::get('/video', 'App\Http\Controllers\HomeController@watchVideo')->name("home.watchVideo");
-// Route::get('/index', 'App\Http\Controllers\HomeController@index')->name("home.index");
-// Route::get('/users', 'App\Http\Controllers\HomeController@selectUser')->name("home.selectUser");
-
-
-
-
 Route::get('/', 'App\Http\Controllers\HomeController@index');
 Route::get('/usuario', 'App\Http\Controllers\HomeController@user')->name('user');
 Route::get('/planes', 'App\Http\Controllers\HomeController@planes')->name('planes');
 Route::get('/letra', 'App\Http\Controllers\HomeController@letra')->name('letra');
 Route::get('/emergencia', 'App\Http\Controllers\HomeController@emergencia')->name('emergencia');
 
-// Route::get('/login/users', 'App\Http\Controllers\AuthController@login')->name("login.users");
 Route::get('/registro/paciente', 'App\Http\Controllers\AuthController@registerPatient')->name("register.patient");
 Route::get('/registro/doctor', 'App\Http\Controllers\AuthController@registerDoctor')->name("register.doctor");
 Route::get('/registro/acompanante', 'App\Http\Controllers\AuthController@registerCuidador')->name("register.cuidador");
@@ -45,12 +36,9 @@ Route::get('/paciente/actividades/save', 'App\Http\Controllers\ActivityControlle
 Route::get('/paciente/actividades/{id}', 'App\Http\Controllers\ActivityController@show')->name("activities.show");
 
 Route::get('/acompanante/index', 'App\Http\Controllers\CuidadorController@index')->name('cuidador.index');
+Route::get('/acompanante/actividades', 'App\Http\Controllers\CuidadorController@activities')->name('cuidador.actividades');
 
-// Route::get('/patient/emergencia', 'App\Http\Controllers\HomeController@emergencia')->name("home.emergencia");
-// Route::get('/paciente/intensidad', 'App\Http\Controllers\PatientController@intensidad');
-
-
-// Route::get('/doctor/patients', 'App\Http\Controllers\UserController@index')->name("doctor.user.index");
-// Route::get('/doctor/patients/{id}', 'App\Http\Controllers\UserController@show')->name("doctor.user.show");
+Route::get('/doctor/pacientes', 'App\Http\Controllers\DoctorController@index')->name("doctor.pacientes");
+Route::get('/doctor/pacientes/{id}', 'App\Http\Controllers\DoctorController@show')->name("doctor.show");
 
 Auth::routes();
