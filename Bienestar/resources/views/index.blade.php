6920ebec014c3227edffb4a2c51da5335da2b04b
Daniela Nino <danielanino194@gmail.com>
2022-11-17 23:27:41 -0500

    Route login

--- Bienestar/resources/views/index.blade.php
+++ Bienestar/resources/views/index.blade.php
@@ -23,7 +23,7 @@
         </div>
         <div class="boton2">
             <br>
-            <a id="boton2" href="#">YA TENGO CUENTA</a>
+            <a id="boton2" href="{{ route('login') }}">YA TENGO CUENTA</a>
         </div>
     </div>
 </body>
