0729b55c2acec46ba481e592ca60ec73b0ad0573
Daniela Nino <danielanino194@gmail.com>
2022-11-17 23:17:19 -0500

    Layout

--- Bienestar/resources/views/layouts/app.blade.php
+++ Bienestar/resources/views/layouts/app.blade.php
@@ -1,36 +1,40 @@
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="{{ asset('/css/layout.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <title>BienEstar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://secure.exportkit.com/cdn/js/ek_googlefonts.js?v=6"></script>
 </head>
 
 <body>
     <nav class="navbar navbar-light nav-color">
-    <a class="" href="#">
-        <button class="sos" type="button">
-            <b>SOS</b>
-        </button>
-    </a>
-    
-        <a class="bienestar" href="#"><b class="txt-lg">BIENESTAR</b><br>ESTAR BIEN</a>
-
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon">
             </span>
         </button>
+
+        <div class="bienestar"><b class="txt-lg">BIENESTAR</b><br>ESTAR BIEN</div>
+
+        <a class="" href="{{ route('emergencia') }}">
+            <button class="sos" type="button">
+                <b>SOS</b>
+            </button>
+        </a>
+
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-                <li class="nav-item active"> <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Features</a> </li>
-                <li class="nav-item"> <a class="nav-link" href="#">Pricing</a> </li>
-                <li class="nav-item"> <a class="nav-link disabled" href="#">Disabled</a></li>
+                <li class="nav-item"> <a class="nav-link" href="{{ route('home.test') }}">Realizar prueba</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="{{ route('test.resultados') }}">Resultados</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Actividades</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="#">Progreso</a> </li>
+                <li class="nav-item"> <a class="nav-link" href="{{ route('calendario') }}">Calendario</a> </li>
+
             </ul>
         </div>
     </nav>
     @yield('content')
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
