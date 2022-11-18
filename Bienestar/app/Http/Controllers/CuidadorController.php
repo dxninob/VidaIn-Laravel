50ba61ef08f5a3b4327ac1ae98a26ffddaa01fed
Daniela Nino <danielanino194@gmail.com>
2022-11-18 04:32:11 -0500

    Cuidador controller

--- Bienestar/app/Http/Controllers/CuidadorController.php
+++ Bienestar/app/Http/Controllers/CuidadorController.php
@@ -9,23 +9,22 @@ use App\Models\Activity;
 
 class CuidadorController extends Controller
 {
-
-    // public function activities($id)
-    // {
-    //     $user = User::findOrFail($id);
-    //     $activities= DB::table('user_activity')->where('user_id', $id)->pluck('activity_id')->toArray();
-    //     $viewData = [];
-    //     $activities_array = [];
-    //     foreach($activities as $act) {
-    //         $act_obj = Activity::findOrFail($act);
-    //         array_push($activities_array, $act_obj);
-    //     }
-    //     $viewData["activities"] = $activities_array;
-    //     return view('cuidador.activities')->with("viewData", $viewData);
-    // }
-
     public function index()
     {
         return view('cuidador.index');
+    }
+
+    public function activities($id)
+    {
+        $user = User::findOrFail($id);
+        $activities= DB::table('user_activity')->where('user_id', $id)->pluck('activity_id')->toArray();
+        $viewData = [];
+        $activities_array = [];
+        foreach($activities as $act) {
+            $act_obj = Activity::findOrFail($act);
+            array_push($activities_array, $act_obj);
+        }
+        $viewData["activities"] = $activities_array;
+        return view('cuidador.activities')->with("viewData", $viewData);
     }
 }
